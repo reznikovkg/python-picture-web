23f118662673583059fbbe06ad4a1edd2f3d243d
spartak0 <spartak-mishin@ya.ru>
2024-10-03 18:52:00 +0300

    Init routing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <RouterLink :to="{ name: ROUTES.HOME}">Home</RouterLink> |
+      <RouterLink :to="{ name: ROUTES.LOGIN}">Login</RouterLink> |
+      <RouterLink :to="{ name: ROUTES.LIST}">List</RouterLink>
+    </nav>
+    <RouterView/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ROUTES } from '@/router'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    ROUTES (){
+      return ROUTES
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
