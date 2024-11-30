e9ee80b18edb0e5b48ee1edb2c969ed410f82b30
julianatelkova <telkova.ysia@mail.ru>
2024-11-30 12:45:31 +0300

    fix image response

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,19 +3,19 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/auth': {
-        target: 'http://localhost:8000',
+        target: 'http://back:8000',
         changeOrigin: true,
       },
       '^/back/classification-image': {
-        target: 'http://localhost:8000',
+        target: 'http://back:8000',
         changeOrigin: true,
       },
       '^/cnn_table': {
-        target: 'http://localhost:8000',
+        target: 'http://back:8000',
         changeOrigin: true,
       },
       '^/main/media/images/(.*)': {
-        target: 'http://localhost:8000',
+        target: 'http://back:8000',
         changeOrigin: true,
       }
     }
